676f503e88013b4a21af15caed4ee7b34b3bc85c
akarac <adnan@p3rsonal.com>
2012-10-25 17:06:40 +0200

    - added callback route

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Notes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
      resources :notes
      resources :oauth2
+     match 'oauth2/callback'
 
   # Sample resource route with options:
   #   resources :products do
